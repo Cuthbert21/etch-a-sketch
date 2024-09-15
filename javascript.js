dc6293cdd3466133d4fd45a604e5c2033825ad36
Kiryl21 <kirillevst1@gmail.com>
2024-09-16 00:03:01 +0200

    Add the button to clear the workfield and change the dots size

--- javascript.js
+++ javascript.js
@@ -13,3 +13,27 @@ for (let i=0; i<256; i++){
     container.appendChild(div);
 
 }
+
+const btn = document.querySelector("#btn");
+    btn.addEventListener("click", () => {
+    let input = parseInt(prompt("Enter a number between 1-100", "16"));
+    let result = input * input;
+    let size = 480 / input;
+    container.innerHTML = '';
+
+    for (let i=0; i<result; i++){
+
+        const div = document.createElement("div");
+    
+        div.className = "dots"
+
+        div.setAttribute("style", `height: ${size}px; width: ${size}px;`);
+    
+        div.addEventListener("mouseover", (event) => {
+            div.style.backgroundColor = "black"
+        });
+    
+        container.appendChild(div);
+    
+    }
+});
